ca7aa4a8e678c0603a9393a94e1316bdbcfb0ad1
Oleh Karnaukhov <karnauhov.oleg@gmail.com>
2026-05-06 08:03:44 +0200

    Deploy app 1.0.0 (29)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -151,7 +151,7 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
   _flutter.loader.load({
     serviceWorkerSettings: {
       serviceWorkerVersion: parseServiceWorkerVersion(
-        `"3549617893" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */`,
+        `"3059640982" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */`,
       ),
     },
     onEntrypointLoaded: async (engineInitializer) => {
