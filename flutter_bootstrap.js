c6ccd428f56d0b6e4af908ff4065f9433375ceba
Oleh Karnaukhov <karnauhov.oleg@gmail.com>
2026-08-18 09:45:54 +0200

    Deploy app 1.1.0 (76)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -152,7 +152,7 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
   _flutter.loader.load({
     serviceWorkerSettings: {
       serviceWorkerVersion: parseServiceWorkerVersion(
-        `"549327431"`,
+        `"2258754510"`,
       ),
     },
     onEntrypointLoaded: async (engineInitializer) => {
